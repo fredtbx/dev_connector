0270e9297eabe80de19602536efde1a415f24659
Fred <fredtbx@comcast.net>
2019-05-30 08:59:21 -0400

    routine save

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,9 @@
 import React, { Fragment, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import DashboardActions from './DashboardActions';
 import { getCurrentProfile } from '../../actions/profile';
 
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
@@ -11,7 +13,14 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
   return loading && profile === null ? <Spinner /> : <Fragment>
     <h1 className="large text-primary">Dashboard</h1>
     <p className="lead"><i className="fas fa-user"></i>Welcome { user && user.name }</p>
-    {profile !== null ? <Fragment>HAS</Fragment> : <Fragment>has NOT</Fragment>}
+    {profile !== null ? <Fragment><DashboardActions /></Fragment> 
+      : 
+    <Fragment>
+      <p>You have not set up a profile yet, please add some info</p>
+      <Link to='/create-profile' className="btn btn-primary my-1">
+        CREATE PROFILE
+      </Link>
+    </Fragment>}
     </Fragment>
 };
 
