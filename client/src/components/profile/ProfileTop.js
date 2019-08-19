a48beb3b9236d2f9a258b4cca71129b125baf8d9
fredtbx <fredtbx@comcast.net>
2019-08-19 16:11:39 -0400

    Update ProfileTop.js

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -19,7 +19,7 @@ const ProfileTop = ({
             alt=""
           />
           <h1 className="large">{name}</h1>
-          <p className="lead">{status} {company && <span> at {company}</span>} at Microsoft</p>
+          <p className="lead">{status} {company && <span> at {company}</span>}</p>
           <p>{location && <span>{location}</span>}</p>
           <div className="icons my-1">
             {website && (
